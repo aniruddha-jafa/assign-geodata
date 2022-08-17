797c747e8cb5d6b8b178ea88fee7b2a7275a685d
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-08-17 17:35:56 +0530

    fixed unit test

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -6,10 +6,9 @@ class CustomUserTestCase(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            name="test", email="test@email.com", password="test12345"
+            username="test@email.com", password="test12345"
         )
-        self.assertEqual(user.name, 'test')
-        self.assertEqual(user.email, 'test@email.com')
+        self.assertEqual(user.username, 'test@email.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
