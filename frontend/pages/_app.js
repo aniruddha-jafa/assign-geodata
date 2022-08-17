8194b7f33692127b5c06cd02fb0824c4e523b823
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-08-17 14:35:25 +0530

    integrate error boundary & pure css

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import ErrorBoundary from '../components/ErrorBoundary'
+import 'purecss/build/pure-min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet='UTF-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0'
+                />
+            </Head>
+            <ErrorBoundary>
+                <Component {...pageProps} />
+            </ErrorBoundary>
+        </>
+    )
 }
 
 export default MyApp
